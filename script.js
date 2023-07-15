29ca4a5011dd836b37a543e7ba74ec2b1311cc3b
mohitverse <mohit@prabidhilabs.com>
2023-07-15 23:19:59 +0545

    Added :: autoPlay

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ document.getElementById(
 
 document.addEventListener('DOMContentLoaded', function () {
   var carousel = new Flickity('.carousel', {
-    // Flickity options here
     cellAlign: 'left',
     contain: true,
     prevNextButtons: false,
     pageDots: false,
     wrapAround: true,
+    autoPlay: 4000, // Initial autoplay duration
+    pauseAutoPlayOnHover: false,
+    draggable: false, // Disable dragging
+  });
+
+  function playCarousel() {
+    carousel.next();
+  }
+
+  var autoplayInterval = setInterval(playCarousel, 4000); // Interval between slide transitions
+
+  carousel.on('dragStart', function () {
+    clearInterval(autoplayInterval); // Clear the autoplay interval when dragging starts
+  });
+
+  carousel.on('dragEnd', function () {
+    autoplayInterval = setInterval(playCarousel, 4000); // Reset the autoplay interval when dragging ends
   });
 });
