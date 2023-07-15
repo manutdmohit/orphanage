4df8589b36d24e632df31fc4dd361c49019e1ff0
mohitverse <mohit@prabidhilabs.com>
2023-07-15 22:51:51 +0545

    Added :: Carousel // Need to work on prev next button

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const currentYear = new Date().getFullYear();
 document.getElementById(
   'footer-year'
 ).textContent = `© ${currentYear} VWF. All rights reserved.`;
+
+document.addEventListener('DOMContentLoaded', function () {
+  var carousel = new Flickity('.carousel', {
+    // Flickity options here
+    cellAlign: 'left',
+    contain: true,
+    prevNextButtons: false,
+    pageDots: false,
+  });
+});
