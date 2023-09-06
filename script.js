7094bfeb203aa7ea3a05e87fa6b8e361791893c8
Mohit Saud <mohit@prabidhilabs.com>
2023-09-06 23:33:54 +0545

    Add Slider to images

--- script.js
+++ script.js
@@ -3,29 +3,72 @@ document.getElementById(
   'footer-year'
 ).textContent = `© ${currentYear} VWF. All rights reserved.`;
 
-document.addEventListener('DOMContentLoaded', function () {
-  var carousel = new Flickity('.carousel', {
-    cellAlign: 'left',
-    contain: true,
-    prevNextButtons: true,
-    pageDots: false,
-    wrapAround: true,
-    autoPlay: 4000, // Initial autoplay duration
-    pauseAutoPlayOnHover: false,
-    draggable: false, // Disable dragging
-  });
+let slideIndex = 0;
+showSlides();
 
-  function playCarousel() {
-    carousel.next();
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('dot');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
-  var autoplayInterval = setInterval(playCarousel, 4000); // Interval between slide transitions
+// const tableContainer = document.querySelector('.table-container');
+// const tableHeader = document.querySelector('thead');
 
-  carousel.on('dragStart', function () {
-    clearInterval(autoplayInterval); // Clear the autoplay interval when dragging starts
-  });
+// tableContainer.addEventListener('scroll', function () {
+//   if (tableContainer.scrollTop > 0) {
+//     tableHeader.style.transform = `translateY(${tableContainer.scrollTop}px)`;
+//   } else {
+//     tableHeader.style.transform = 'translateY(0)';
+//   }
+// });
 
-  carousel.on('dragEnd', function () {
-    autoplayInterval = setInterval(playCarousel, 4000); // Reset the autoplay interval when dragging ends
-  });
-});
+// // Get the table container element
+// const tableContainer = document.querySelector('.sticky-table-container');
+
+// // Get the table element
+// const table = document.querySelector('.sticky-table');
+
+// // Add a scroll event listener to the table container
+// tableContainer.addEventListener('scroll', function () {
+//   // Calculate the scroll position
+//   const scrollTop = tableContainer.scrollTop;
+
+//   // Check if the scroll position is greater than the offset of the table header
+//   if (scrollTop >= table.offsetTop) {
+//     // Add the "sticky" class to the table to apply extra padding to td elements
+//     table.classList.add('sticky');
+//   } else {
+//     // Remove the "sticky" class when not scrolled
+//     table.classList.remove('sticky');
+//   }
+// });
+
+// Get the table element by its id
+// const table = document.getElementById('myTable');
+
+// // Add an event listener for scroll events
+// window.addEventListener('scroll', () => {
+//   const rect = table.getBoundingClientRect();
+//   if (rect.top < 0) {
+//     table.querySelector('thead').style.transform = `translateY(${Math.min(
+//       0,
+//       rect.top
+//     )}px)`;
+//   } else {
+//     table.querySelector('thead').style.transform = '';
+//   }
+// });s
