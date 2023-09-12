9bab723d6a193c96b7d7ac1e739dab9ecf3ce569
Mohit Saud <mohit@prabidhilabs.com>
2023-09-12 16:35:49 +0545

    Fixed :: main element not showing on opening icon

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ document.getElementById(
 document.addEventListener('DOMContentLoaded', function () {
   const hamburgerIcon = document.querySelector('.hamburger-icon');
   const navLinks = document.querySelector('.nav-links');
+  const mainContent = document.querySelector('main');
+
+  const aboutUs = document.querySelector('#about-us');
+  // aboutUs.addEventListener('click', function () {
+  //   console.log('About us clicked');
+  // });
 
   hamburgerIcon.addEventListener('click', function () {
     navLinks.classList.toggle('active');
+    mainContent.classList.toggle('main-menu-active-padding');
+  });
+
+  aboutUs.addEventListener('click', function () {
+    console.log('About us clicked');
+    navLinks.classList.toggle('active');
+    mainContent.classList.toggle('main-menu-active-padding');
   });
 });
 
@@ -23,12 +36,15 @@ function showSlides() {
     slides[i].style.display = 'none';
   }
   slideIndex++;
+
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(' active', '');
   }
+
   slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' active';
   setTimeout(showSlides, 2000); // Change image every 2 seconds
